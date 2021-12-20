95f4d00f881236fad647e3eb22bcb36facae269c
ohju96 <ohju96@naver.com>
2021-12-20 18:15:03 +0900

    암호화, 복호화 알고리즘 적용

--- Parking_management/src/poly/util/EncryptUtil.java
+++ Parking_management/src/poly/util/EncryptUtil.java
@@ -1,7 +1,20 @@
 package poly.util;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.AlgorithmParameterSpec;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.tomcat.util.codec.binary.Base64;
 
 public class EncryptUtil {
 
@@ -14,9 +27,17 @@ public class EncryptUtil {
 	final static String addMessage = "ohjuhyeon";
 
 	/*
+	 *--------------------------------------------------------------------------------------------
 	 * AES128-CBC 암호화 알고리즘에 사용되는 초기 백터와 암호화 키
 	 */
-
+	
+	// 초기 백터 (16Byte 크기를 가지며, 16Byte 단위로 암호화 시, 암호화 할 총 길이가 16Byte가 되지 못 하면 뒤에 추가하는 바이트)
+	final static byte[] ivBytes = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+	
+	//AES128-CBC 암호화 알고리즘에 사용되는 키 (16자리 문자만 가능하다.)
+	final static String key = "Ohjuhyeon1234567"; // 16글자가 필요하다. (영문 1글자당 1Byte)
+	//--------------------------------------------------------------------------------------------
+	
 	/*
 	 * 해시 알고리즘 ( 단방향 암호화 알고리즘 ) SHA-256
 	 * 
@@ -57,6 +78,43 @@ public class EncryptUtil {
 		}
 
 		return res;
+	}
+	
+	//--------------------------------------------------------------------------------------------
+	/*
+	 * AES128 CBC 암호화 함수
+	 *  128은 암호화 키 길이를 의미한다. 
+	 *  128bit = 16byte (1Byte=8bit * 16 = 128)
+	 */
+	public static String encAES128CBC(String str)
+		throws UnsupportedEncodingException, NoSuchAlgorithmException,NoSuchPaddingException,
+		InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+		
+		byte[] textBytes = str.getBytes("UTF-8");
+		AlgorithmParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+		SecretKeySpec newKey = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+		Cipher cipher = null;
+		cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, newKey, ivSpec);
+		return Base64.encodeBase64String(cipher.doFinal(textBytes));
+	}
+	
+	/*
+	 * AES128 CBC 복호화 함수
+	 *  128은 암호화 키 길이를 의미한다. 
+	 *  128bit = 16byte (1Byte=8bit * 16 = 128)
+	 */
+	public static String decAES128CBC(String str)
+		throws UnsupportedEncodingException, NoSuchAlgorithmException,NoSuchPaddingException,
+		InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
+	
+		byte[] textBytes = Base64.decodeBase64(str);
+		AlgorithmParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+		SecretKeySpec newKey = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+		cipher.init(Cipher.DECRYPT_MODE, newKey, ivSpec);
+		return new String(cipher.doFinal(textBytes), "UTF-8");
 
 	}
+			 
 }
