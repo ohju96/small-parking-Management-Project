23e93d47d502cb3c08d79802a71ac9cf3d99cbee
ohju96 <ohju96@naver.com>
2021-12-22 17:25:53 +0900

    User 데이터 구축 및 프로젝트 DTO 등록

---
+++ Parking_management/src/poly/dto/UserDTO.java
@@ -0,0 +1,45 @@
+package poly.dto;
+
+/*
+ * @author 오주현
+ * @version 0.1 user_info DTO
+ */
+public class UserDTO {
+
+	private String user_seq;
+	private String user_email;
+	private String user_pw;
+	private String user_name;
+	private String user_address;
+	
+	public String getUser_seq() {
+		return user_seq;
+	}
+	public void setUser_seq(String user_seq) {
+		this.user_seq = user_seq;
+	}
+	public String getUser_email() {
+		return user_email;
+	}
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
+	}
+	public String getUser_pw() {
+		return user_pw;
+	}
+	public void setUser_pw(String user_pw) {
+		this.user_pw = user_pw;
+	}
+	public String getUser_name() {
+		return user_name;
+	}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+	public String getUser_address() {
+		return user_address;
+	}
+	public void setUser_address(String user_address) {
+		this.user_address = user_address;
+	}
+}
