e5efbd0d8b618a18eed9f47f471a0ade0056d10a
ohju96 <ohju96@naver.com>
2021-12-24 16:23:09 +0900

    log4j 관련 보안 취약점 때문에 프로젝트 교체

--- Parking_management/src/poly/service/impl/NoticeService.java
+++ Parking_management/src/poly/service/impl/NoticeService.java
@@ -9,46 +9,47 @@ import org.springframework.stereotype.Service;
 import poly.dto.NoticeDTO;
 import poly.persistance.mapper.NoticeMapper;
 import poly.service.INoticeService;
+import poly.service.comm.AbstractService;
 
 @Service("NoticeService")
-public class NoticeService implements INoticeService{
-	
-	@Resource(name="NoticeMapper")
+public class NoticeService extends AbstractService implements INoticeService {
+
+	@Resource(name = "NoticeMapper")
 	private NoticeMapper noticeMapper;
-	
+
 	@Override
 	public List<NoticeDTO> getNoticeList() throws Exception {
 		return noticeMapper.getNoticeList();
-		
+
 	}
-	
+
 	@Override
 	public void InsertNoticeInfo(NoticeDTO pDTO) throws Exception {
-		noticeMapper.InsertNoticeInfo(pDTO);;
-		
+		noticeMapper.InsertNoticeInfo(pDTO);
+
 	}
-	
+
 	@Override
 	public NoticeDTO getNoticeInfo(NoticeDTO pDTO) throws Exception {
 		return noticeMapper.getNoticeInfo(pDTO);
-		
+
 	}
-	
+
 	@Override
 	public void updateNoticeReadCnt(NoticeDTO pDTO) throws Exception {
-		noticeMapper.updateNoticeReadCnt(pDTO);;
-		
+		noticeMapper.updateNoticeReadCnt(pDTO);
+
 	}
-	
+
 	@Override
 	public void updateNoticeInfo(NoticeDTO pDTO) throws Exception {
 		noticeMapper.updateNoticeInfo(pDTO);
-		
+
 	}
-	
+
 	@Override
 	public void deleteNoticeInfo(NoticeDTO pDTO) throws Exception {
 		noticeMapper.deleteNoticeInfo(pDTO);
-		
+
 	}
 }
