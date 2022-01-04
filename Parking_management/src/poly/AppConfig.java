34e3d96cb4f72c397e2262eb90a434424c213e28
ohju96 <ohju96@naver.com>
2022-01-04 21:34:10 +0900

    MongoDB 셋팅 준비

---
+++ Parking_management/src/poly/AppConfig.java
@@ -0,0 +1,13 @@
+package poly;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// DI 컨테이너
+@Configuration 
+public class AppConfig {
+	
+	//@Bean
+	
+	
+}
