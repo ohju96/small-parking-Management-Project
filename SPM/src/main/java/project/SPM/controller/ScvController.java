f023d4815d69cd62d0ec6d85b84d1692150df9dd
ohju96 <ohju96@naver.com>
2022-05-07 16:09:30 +0900

    poi라이브러리로 엑셀 데이터 읽고 리스트에 담아 mongodb에 저장하기

--- SPM/src/main/java/project/SPM/controller/ScvController.java
+++ SPM/src/main/java/project/SPM/controller/ScvController.java
@@ -1,2 +1,56 @@
-package project.SPM.controller;public class ScvController {
+package project.SPM.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@Slf4j
+@RestController
+public class ScvController {
+
+    @RequestMapping(value = "/test.do", method = RequestMethod.GET)
+    public ModelAndView Test() {
+        ModelAndView mav = new ModelAndView();
+
+        String url = "http://3.39.156.189:8080/carManagement/csv";
+        String sb = "";
+        try {
+            HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+
+            String line = null;
+
+            while ((line = br.readLine()) != null) {
+                sb = sb + line + "\n";
+            }
+            System.out.println("========br======" + sb.toString());
+            if (sb.toString().contains("ok")) {
+                System.out.println("test");
+
+            }
+            br.close();
+
+            System.out.println("" + sb.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        mav.addObject("test1", sb.toString()); // "test1"는 jsp파일에서 받을때 이름,
+        //sb.toString은 value값(여기에선 test)
+        mav.addObject("fail", false);
+        mav.setViewName("test");   // jsp파일 이름
+        return mav;
+    }
+
 }
