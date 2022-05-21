057d71e618984ba1373f39c286caaf594df86500
ohju96 <ohju96@naver.com>
2022-05-21 23:03:54 +0900

    Check 로직 수정 중

--- SPM/src/main/java/project/SPM/service/ICheckService.java
+++ SPM/src/main/java/project/SPM/service/ICheckService.java
@@ -1,13 +1,14 @@
 package project.SPM.service;
 
 import project.SPM.dto.CarDTO;
+import project.SPM.vo.CheckListVo;
 
 import java.util.List;
 
 public interface ICheckService {
 
     // 직접 저장 로직
-    boolean saveTouchCheck(CarDTO carDTO) throws Exception;
+    boolean saveTouchCheck(CheckListVo checkListVo) throws Exception;
 
     // 이미지 저장 로직
     List<CarDTO> saveImgCheck() throws Exception;
