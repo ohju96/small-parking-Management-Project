8900051c6f4b359fa1f20aac5fd6e8866d50944f
ohju96 <ohju96@naver.com>
2022-06-14 19:04:11 +0900

    restTemplate 추가

--- SPM/src/main/java/project/SPM/web/WebConfig.java
+++ SPM/src/main/java/project/SPM/web/WebConfig.java
@@ -3,6 +3,7 @@ package project.SPM.web;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import project.SPM.filter.LoginCheckFilter;
 
 import javax.servlet.Filter;
@@ -17,5 +18,12 @@ public class WebConfig {
         filterFilterRegistrationBean.setOrder(1); // 적용 순서
         filterFilterRegistrationBean.addUrlPatterns("/*"); //어디에 적용할지?, 화이트 리스트로 걸러줘서 다 넣어도 된다.
         return filterFilterRegistrationBean;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        return restTemplate;
     }
 }
